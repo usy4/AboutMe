41089bba51c3cf392c164f70008cf13fad93cfb5
usy4 <68358524+usy4@users.noreply.github.com>
2022-12-24 03:04:28 +0300

    Update EventListener.php

--- src/usy4/AboutMe/EventListener.php
+++ src/usy4/AboutMe/EventListener.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace usy4\AboutMe;
 
+use pocketmine\event\entity\EntityDamageByEntityEvent;
 use pocketmine\event\entity\EntityDamageEvent;
 use pocketmine\event\Listener;
 use pocketmine\player\Player;
@@ -12,13 +13,20 @@ use usy4\AboutMe\Main;
 
 class EventListener implements Listener {
 
+    /**
+     * @ignoreCancelled true
+      * @priority MONITOR
+     */  
+    
     public function onDamage(EntityDamageEvent $event): void
     {
         $player = $event->getEntity();
         if(!$player instanceof Player) return;
-        if(Main::$config->get("DamageToRead") == false) return;
-        if($player->getWorld()->getFolderName() !== Main::$config->get("WorldName")) return;
-        Main::WriteForm($player);
+        if($event instanceof EntityDamageByEntityEvent && ($damager = $event->getDamager()) instanceof Player){
+            if(Main::$config->get("DamageToRead") == false) return;
+            if($player->getWorld()->getFolderName() !== Main::$config->get("WorldName")) return;
+            Main::ReadForm($damager, $player->getName());
+        }
     }
 
 }
